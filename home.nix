9596fcde900b9ade977e65f9ebddae2cb9811523
phanirithvij <phanirithvij2000@gmail.com>
2024-02-06 16:46:50 +0530

    gitconfig

--- home.nix
+++ home.nix
@@ -64,10 +64,28 @@
     wezterm
   ];
 
+  programs.gh = {
+    enable = true;
+    gitCredentialHelper.enable = true;
+  };
   programs.git = {
     enable = true;
     userName = "phanirithvij";
     userEmail = "phanirithvij2000@gmail.com";
+    delta.enable = true;
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+      url = {
+        "https://github.com/" = {
+          insteadOf = [
+            "gh:"
+            "github:"
+          ];
+        };
+      };
+    };
   };
 
   programs.starship = {
