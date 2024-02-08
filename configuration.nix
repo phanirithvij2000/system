c2b6aa326d4cf9f8a8d5c46f1298a84b827cc99e
phanirithvij <phanirithvij2000@gmail.com>
2024-02-08 07:48:48 +0530

    appimage and koreader

--- configuration.nix
+++ configuration.nix
@@ -29,7 +29,17 @@
       '';
     };
   };
-  boot.binfmt.emulatedSystems = ["aarch64-linux"];
+  boot.binfmt = {
+    emulatedSystems = ["aarch64-linux"];
+    registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+    };
+  };
 
   networking.hostName = "iron";
   networking.networkmanager.enable = true;
--- home.nix
+++ home.nix
@@ -61,6 +61,7 @@
     telegram-desktop
     qbittorrent
     rustdesk
+    koreader
 
     wezterm
   ];
