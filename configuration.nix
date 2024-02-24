ee486637979e04720d08a0f02f8894471352640d
phanirithvij <phanirithvij2000@gmail.com>
2024-02-24 23:12:51 +0530

    podman enable

--- configuration.nix
+++ configuration.nix
@@ -104,6 +104,19 @@
   };
   nixpkgs.config.allowUnfree = true;
 
+  # https://nixos.wiki/wiki/Podman
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
   users.users.rithvij = {
     isNormalUser = true;
     # Hint: my clash of clans username
