25c826151b350f24a48d9fb7ef812f3e4cd7917c
phanirithvij <phanirithvij2000@gmail.com>
2024-05-22 22:56:55 +0530

    topgrade

--- home.nix
+++ home.nix
@@ -127,7 +127,27 @@
   };
 
   programs.tealdeer.enable = true;
-  programs.topgrade.enable = true;
+  programs.topgrade = {
+    enable = true;
+    # https://github.com/topgrade-rs/topgrade/blob/1e9de5832d977f8f89596253f2880760533ec5f5/config.example.toml
+    settings = {
+      misc = {
+        assume_yes = true;
+        disable = [
+          "flutter"
+          "node"
+        ];
+        set_title = false;
+        cleanup = true;
+        run_in_tmux = true;
+        skip_notify = true;
+      };
+      linux = {
+        nix_arguments = "--flake /home/rithvij/Projects/system#iron";
+        home_manager_arguments = ["--flake" "/home/rithvij/Projects/system#rithvij"];
+      };
+    };
+  };
   programs.yt-dlp.enable = true;
   programs.zoxide.enable = true;
 
