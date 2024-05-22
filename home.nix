5df63d7977e65ab26b76d3a9a29a75a838f42070
phanirithvij <phanirithvij2000@gmail.com>
2024-05-22 22:27:20 +0530

    rustdesk appimage pinned

--- home.nix
+++ home.nix
@@ -36,7 +36,6 @@
     tor-browser
     telegram-desktop
     qbittorrent
-    #rustdesk #rustdesk takes up time to compile and UI is bad. I prefer its AppImage on nixos
     koreader
     qimgv
     beekeeper-studio
@@ -245,6 +244,15 @@
     [build]
     rustc-wrapper = "sccache"
   '';
+
+  #rustdesk takes up time to compile and UI is bad. I prefer its AppImage on nixos
+  home.file."Desktop/rustdesk-x86_64.AppImage" = {
+    source = builtins.fetchurl {
+      url = "https://github.com/rustdesk/rustdesk/releases/download/1.2.3-2/rustdesk-1.2.3-2-x86_64.AppImage";
+      sha256 = "309a9be742bc63798064e712d0eb8745987d55f76f32a8d99e2089dba7b0795e";
+    };
+    executable = true;
+  };
 
   home.file.".tmux/resize-hook.sh".source = ./config/tmux/resize-hook.sh;
   home.file.".tmux.conf".source = ./config/tmux/.tmux.conf;
