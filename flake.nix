375cb368abdb15875cc146e25b483a2344592654
phanirithvij <phanirithvij2000@gmail.com>
2024-05-22 19:36:02 +0530

    try nix-schema overlay

--- flake.nix
+++ flake.nix
@@ -11,6 +11,13 @@
     blobdrop.url = "github:vimpostor/blobdrop/ee4eac75d8afa2f68288e462723a29cab5e52c45";
     blobdrop.inputs.nixpkgs.follows = "nixpkgs";
 
+    # https://github.com/gvolpe/nix-config/blob/d983b5e6d8c4d57152ef31fa7141d3aad465123a/flake.nix#L17
+    flake-schemas.url = "github:gvolpe/flake-schemas";
+    ## nix client with schema support: see https://github.com/NixOS/nix/pull/8892
+    nix-schema = {
+      inputs.flake-schemas.follows = "flake-schemas";
+      url = "github:DeterminateSystems/nix-src/flake-schemas";
+    };
     # TODO jupyenv python, nix, go kernels
   };
 
@@ -21,16 +28,23 @@
     home-manager,
     blobdrop,
     ...
-  }: let
+  } @ inputs: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
-      inherit system;
+      inherit overlays system;
       config = {
         allowUnfree = true;
         allowUnfreePredicate = _: true;
       };
     };
+    overlays = import ./lib/overlays.nix {inherit inputs system;};
   in {
+    schemas =
+      inputs.flake-schemas.schemas;
+    apps.${system}."nix" = {
+      type = "app";
+      program = "${pkgs.nix-schema}/bin/nix-schema";
+    };
     homeConfigurations = {
       rithvij = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
---
+++ lib/overlays.nix
@@ -0,0 +1,20 @@
+{
+  inputs,
+  system,
+}:
+with inputs; let
+  schemaOverlay = f: p: {
+    nix-schema = inputs.nix-schema.packages.${system}.nix.overrideAttrs (old: {
+      doCheck = false;
+      doInstallCheck = false;
+      postInstall =
+        old.postInstall
+        + ''
+          rm $out/bin/nix-*
+          mv $out/bin/nix $out/bin/nix-schema
+        '';
+    });
+  };
+in [
+  schemaOverlay
+]
