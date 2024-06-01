7823fbcdac98a6c20474508b081a97f0a162df64
phanirithvij <phanirithvij2000@gmail.com>
2024-06-01 21:28:58 +0530

    fmt

--- nixos/specialisations.nix
+++ nixos/specialisations.nix
@@ -15,23 +15,21 @@
       inheritParentConfig = true;
     };
     ly = {
-      configuration =
-        { ... }:
-        {
-          imports = [ "${inputs.nixpkgs-ly}/nixos/modules/services/display-managers/ly.nix" ];
-          services = {
-            displayManager.ly.enable = true;
-            displayManager.ly.settings = {
-              load = false;
-              save = false;
-            };
-            displayManager.sddm.enable = lib.mkForce false;
-            xserver.enable = true;
-            xserver.displayManager.lightdm.enable = lib.mkForce false;
-            desktopManager.plasma6.enable = lib.mkForce false;
-            flatpak.enable = lib.mkForce false;
+      configuration = {
+        imports = [ "${inputs.nixpkgs-ly}/nixos/modules/services/display-managers/ly.nix" ];
+        services = {
+          displayManager.ly.enable = true;
+          displayManager.ly.settings = {
+            load = false;
+            save = false;
           };
+          displayManager.sddm.enable = lib.mkForce false;
+          xserver.enable = true;
+          xserver.displayManager.lightdm.enable = lib.mkForce false;
+          desktopManager.plasma6.enable = lib.mkForce false;
+          flatpak.enable = lib.mkForce false;
         };
+      };
       inheritParentConfig = true;
     };
   };
