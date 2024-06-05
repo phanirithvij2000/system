21195626c582049770506fe616d7357d40d61705
phanirithvij <phanirithvij2000@gmail.com>
2024-06-05 13:03:59 +0530

    appimg new: subtitlecomposer-git

--- home/appimgs.nix
+++ home/appimgs.nix
@@ -7,4 +7,14 @@ _: {
     };
     executable = true;
   };
+
+  #subtitlecomposer, I need git version because stable is 6months old
+  #TODO write an overlay to build from git rev instead later. For now download from their releases on home-page
+  home.file."Desktop/subtitlecomposer-git-latest-x86_64.AppImage" = {
+    source = builtins.fetchurl {
+      url = "https://downloadcontent.opensuse.org/repositories/home:/maxrd2/AppImage/subtitlecomposer-git-latest-x86_64.AppImage";
+      sha256 = "0z4sldh4jv0niaz1axwqii2l3cy7d8y4sfnrhl695xdsbrm7fr5p";
+    };
+    executable = true;
+  };
 }
