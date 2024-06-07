8f5b8ded9f0d837624a0e83c061c4c51f8a88d83
phanirithvij <phanirithvij2000@gmail.com>
2024-06-07 14:37:54 +0530

    navi per project cheats
    https://github.com/nix-community/home-manager/blob/master/modules/programs/navi.nix
    https://github.com/denisidoro/navi/issues/848#issuecomment-1961121507

--- home/rithvij/bookmarks.nix
+++ home/rithvij/bookmarks.nix
@@ -1,7 +1,28 @@
 { navi_config, pkgs, ... }:
+let
+  navi = pkgs.navi;
+in
 {
   home.packages = [ pkgs.buku ];
-  programs.navi.enable = true;
+  programs.navi = {
+    enable = true;
+    enableBashIntegration = false;
+    enableZshIntegration = false;
+    enableFishIntegration = false;
+  };
+  programs.bash.initExtra = ''
+    if [[ :$SHELLOPTS: =~ :(vi|emacs): ]]; then
+      eval "$(${navi}/bin/navi widget bash | sed 's/--print/--print --path \"\$PWD\"/g')"
+    fi
+  '';
+  programs.zsh.initExtra = ''
+    if [[ $options[zle] = on ]]; then
+      eval "$(${navi}/bin/navi widget zsh | sed 's/--print/--print --path \"\$PWD\"/g')"
+    fi
+  '';
+  programs.fish.shellInit = ''
+    ${navi}/bin/navi widget fish | sed 's/--print/--print --path "$PWD"/g' | source
+  '';
   home.file.".local/share/navi/cheats/phanirithvij__navi" = {
     source = navi_config;
     recursive = true;
