4a660ea861496cd456c135a6dce9da8ec51bdba5
phanirithvij <phanirithvij2000@gmail.com>
2024-06-09 09:14:53 +0530

    try lazygit config
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/rithvij/git.nix
+++ home/rithvij/git.nix
@@ -5,7 +5,33 @@
     pkgs.hub
   ];
   programs = {
-    lazygit.enable = true; # godsend
+    lazygit = {
+      enable = true;
+      settings = {
+        disableStartupPopups = true;
+        git = {
+          commit = {
+            signOff = true;
+          };
+          paging = {
+            colorArg = "always";
+            pager = "${pkgs.delta} --dark --paging=never";
+          };
+        };
+        gui = {
+          showBottomLine = false;
+          showCommandLog = true;
+          showIcons = false;
+          showListFooter = false;
+          showRandomTip = false;
+          theme = {
+            showFileTree = true;
+          };
+        };
+        notARepository = "quit";
+        promptToReturnFromSubprocess = false;
+      };
+    };
     gh = {
       enable = true;
       gitCredentialHelper.enable = true;
