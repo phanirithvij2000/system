91c2ca07c1a0f7daf6c1d9aa5b0861373ba941c9
phanirithvij <phanirithvij2000@gmail.com>
2024-06-09 10:36:15 +0530

    init wf, make repo public
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

---
+++ .github/scripts/nix-serve.sh
@@ -0,0 +1,3 @@
+nix-store --generate-binary-cache-key gha-magic-cache-2 cache-priv-key.pem cache-pub-key.pem
+export NIX_SECRET_KEY_FILE=$PWD/cache-priv-key.pem
+nix run nixpkgs#nix-serve -- --listen :5000 --daemonize
