d6ce092e4293e9718f13aa6e321d1068780521f7
phanirithvij <phanirithvij2000@gmail.com>
2024-06-12 08:07:01 +0530

    no need for 32bit and alsa
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- hosts/iron/configuration.nix
+++ hosts/iron/configuration.nix
@@ -94,8 +94,6 @@ in
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
