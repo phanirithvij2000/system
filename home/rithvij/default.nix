b998bb27e445875eef28fa066c69677524a36bb6
phanirithvij <phanirithvij2000@gmail.com>
2024-06-25 09:55:37 +0530

    treefmt dprint (buggy todo whatodo)
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/rithvij/default.nix
+++ home/rithvij/default.nix
@@ -40,7 +40,7 @@ in
     yq
     viddy
     duf
-    dprint
+    # dprint # use treefmt with it per project from now on
 
     #joplin # slow node tui app
 
--- treefmt.nix
+++ treefmt.nix
@@ -3,4 +3,16 @@
   projectRootFile = "flake.nix";
   programs.nixfmt-rfc-style.enable = true;
   programs.dprint.enable = true;
+  programs.dprint.settings = {
+    includes = [ "**/*.{toml,json,md}" ];
+    excludes = [
+      "**/node_modules"
+      "**/*-lock.json"
+    ];
+    plugins = [
+      "https://plugins.dprint.dev/json-0.19.3.wasm"
+      "https://plugins.dprint.dev/markdown-0.17.1.wasm"
+      "https://plugins.dprint.dev/toml-0.6.2.wasm"
+    ];
+  };
 }
