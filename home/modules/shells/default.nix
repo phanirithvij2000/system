65b741179b0073dc188200837c75ca1f887f327b
phanirithvij <phanirithvij2000@gmail.com>
2024-06-25 11:59:51 +0530

    use fd abspath
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/modules/shells/default.nix
+++ home/modules/shells/default.nix
@@ -1,18 +1,20 @@
-_: {
-
+{ pkgs, ... }:
+{
   imports = [
     ./bash.nix
     ./fish.nix
   ];
 
   programs.eza.enable = true;
-  programs.fd.enable = true;
+  programs.fd = {
+    enable = true;
+    package = pkgs.fd;
+  };
   programs.fzf = {
     enable = true;
-    defaultCommand = "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git";
-    fileWidgetCommand = "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git";
+    defaultCommand = "${pkgs.fd}/bin/fd --type f --strip-cwd-prefix --hidden --follow --exclude .git";
+    fileWidgetCommand = "${pkgs.fd}/bin/fd --type f --strip-cwd-prefix --hidden --follow --exclude .git";
   };
   programs.lf.enable = true;
   home.file.".config/lf".source = ../config/lf;
-
 }
