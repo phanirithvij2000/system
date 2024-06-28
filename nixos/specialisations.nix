766a3f6dc4e6471f74f533dff8e50d1cc802834d
phanirithvij <phanirithvij2000@gmail.com>
2024-06-28 08:59:30 +0530

    xfce: more pkgs
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/specialisations.nix
+++ nixos/specialisations.nix
@@ -17,6 +17,13 @@
         };
         services.xserver.desktopManager.xfce.enable = true;
         services.desktopManager.plasma6.enable = lib.mkForce false;
+        environment.systemPackages = with pkgs; [
+          plata-theme
+          rofi
+          xfce.thunar
+          xfce.xfconf
+          xfce.xfce4-whiskermenu-plugin
+        ];
       };
       inheritParentConfig = true;
     };
