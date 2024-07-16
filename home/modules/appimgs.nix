adaf9e21d42bf27c38212cb14d9ab4b68ea610c5
phanirithvij <phanirithvij2000@gmail.com>
2024-07-16 10:48:19 +0530

    upd appimg rd
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/modules/appimgs.nix
+++ home/modules/appimgs.nix
@@ -2,8 +2,8 @@ _: {
   #rustdesk takes up time to compile and UI is bad. I prefer its AppImage on nixos
   home.file."Desktop/rustdesk-x86_64.AppImage" = {
     source = builtins.fetchurl {
-      url = "https://github.com/rustdesk/rustdesk/releases/download/1.2.3-2/rustdesk-1.2.3-2-x86_64.AppImage";
-      sha256 = "309a9be742bc63798064e712d0eb8745987d55f76f32a8d99e2089dba7b0795e";
+      url = "https://github.com/rustdesk/rustdesk/releases/download/1.2.6/rustdesk-1.2.6-x86_64.AppImage";
+      sha256 = "0k5sj646qhpaf711z3pw542psf6yjyfvd05gp0f4xv3wcxq6cy41";
     };
     executable = true;
   };
