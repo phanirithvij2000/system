9cc8c418afe76508c2c7ef0e5ec043a9c236e2b1
phanirithvij <phanirithvij2000@gmail.com>
2024-08-01 04:54:55 +0530

    nix-serve sops hmm [skip ci]
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/applications/nix/nixserve/default.nix
+++ nixos/applications/nix/nixserve/default.nix
@@ -1,14 +1,15 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   # TODO
   # attic
   # harmonia
   # nix-serve-ng
+  sops.secrets.nix-serve-privkey = { };
   services.nix-serve = {
     enable = true;
     package = pkgs.nix-serve-ng;
     port = 5000;
     openFirewall = false; # hosted in tailnet so firewall needs tailnet to be trusted
-    secretKeyFile = "${./iron-priv.key}";
+    secretKeyFile = config.sops.secrets.nix-serve-privkey.path;
   };
 }
