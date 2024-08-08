bf0d43b501685d56ac1022f6d55c92bd7b0d2b7a
phanirithvij <phanirithvij2000@gmail.com>
2024-08-08 11:19:07 +0530

    wezterm remove old script
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/config/wezterm/toggle_tab_bar.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-grep -q 'config.enable_tab_bar = false' ~/.config/wezterm/wezterm.lua && sed -i 's/config.enable_tab_bar = false/config.enable_tab_bar = true/' ~/.config/wezterm/wezterm.lua || sed -i 's/config.enable_tab_bar = true/config.enable_tab_bar = false/' ~/.config/wezterm/wezterm.lua
