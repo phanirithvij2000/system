6759dcf3b1fdf20a59c0e0ca4814c36adeaa56c2
phanirithvij <phanirithvij2000@gmail.com>
2024-08-10 11:19:03 +0530

    not anymore
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- treefmt.nix
+++ treefmt.nix
@@ -3,6 +3,5 @@ _: {
   programs.nixfmt.enable = true;
   #programs.dprint.enable = true;
   programs.shfmt.enable = true;
-  # this will always fail, run it once in a while and fix issues in shell scripts
   programs.shellcheck.enable = true;
 }
