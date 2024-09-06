8dc19e4e80dbc3ec83441dc4b8a3b4852cd78f2e
phanirithvij <phanirithvij2000@gmail.com>
2024-09-06 11:47:47 +0530

    fix syncplay sysm
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- hosts/sysm/modules/syncplay.nix
+++ hosts/sysm/modules/syncplay.nix
@@ -5,7 +5,11 @@
   ...
 }:
 {
-  imports = map (path: nixosModulesPath + path) [ "/services/networking/syncplay.nix" ];
+  imports = map (path: nixosModulesPath + path) [
+    "/services/networking/syncplay.nix"
+    "/services/networking/firewall.nix"
+    "/services/networking/nftables.nix"
+  ];
   config = {
     # https://github.com/numtide/system-manager/blob/main/nix/modules/upstream/nixpkgs/nginx.nix
     systemd.services.syncplay = lib.mkIf config.services.syncplay.enable {
