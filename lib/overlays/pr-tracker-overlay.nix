0304392f3756cbaed144f96f7481c08fed5a404c
phanirithvij <phanirithvij2000@gmail.com>
2024-09-13 15:07:30 +0530

    nixpkgs input upd + fix pr-tracker
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- lib/overlays/pr-tracker-overlay.nix
+++ lib/overlays/pr-tracker-overlay.nix
@@ -1,5 +1,5 @@
 _: p: {
   pr-tracker = p.pr-tracker.overrideAttrs (_: {
-    patches = [ ./matt2432-pr-tracker-c9d0fd535b9ad1b53c212a87e0710d55d8b7f42e.patch ];
+    patches = [ ./pr-tracker-gh-token.patch ];
   });
 }
