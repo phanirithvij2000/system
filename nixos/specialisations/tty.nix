18a3aaeb88d765c3f2b48ef768ad5b182a26cb41
phanirithvij <phanirithvij2000@gmail.com>
2024-09-27 23:05:26 +0530

    temp disable tty specialisation
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/specialisations/tty.nix
+++ nixos/specialisations/tty.nix
@@ -1,21 +1,22 @@
-{ modulesPath, lib, ... }:
-{
-  specialisation.tty = {
-    inheritParentConfig = true;
-    configuration = {
-      # The bug below with noXlibs occurs due to importing minimal profile
-      # it fails to compile ghc-8.6
-      imports = [ "${modulesPath}/profiles/minimal.nix" ];
-      hardware.opentabletdriver.enable = lib.mkForce false;
-      # https://github.com/NixOS/nixpkgs/issues/102137
-      # noXlibs has been deprecated forcefully, disable it
-      # environment.noXlibs = lib.mkForce false;
-      services = {
-        xserver.enable = lib.mkForce false;
-        displayManager.ly.enable = lib.mkForce false;
-        desktopManager.plasma6.enable = lib.mkForce false;
-        # TODO disable graphical profile
+_: {
+  /*
+    specialisation.tty = {
+      inheritParentConfig = true;
+      configuration = {
+        # The bug below with noXlibs occurs due to importing minimal profile
+        # it fails to compile ghc-8.6
+        imports = [ "${modulesPath}/profiles/minimal.nix" ];
+        hardware.opentabletdriver.enable = lib.mkForce false;
+        # https://github.com/NixOS/nixpkgs/issues/102137
+        # noXlibs has been deprecated forcefully, disable it
+        # environment.noXlibs = lib.mkForce false;
+        services = {
+          xserver.enable = lib.mkForce false;
+          displayManager.ly.enable = lib.mkForce false;
+          desktopManager.plasma6.enable = lib.mkForce false;
+          # TODO disable graphical profile
+        };
       };
     };
-  };
+  */
 }
