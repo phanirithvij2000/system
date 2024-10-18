9b1f4a1435edde51f7aeb1ec5f5756fdea0b01a1
phanirithvij <phanirithvij2000@gmail.com>
2024-10-18 14:25:45 +0530

    sysm more tools on gha
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- hosts/sysm/gha/configuration.nix
+++ hosts/sysm/gha/configuration.nix
@@ -6,15 +6,35 @@
   ];
   config = {
     nixpkgs.hostPlatform = "x86_64-linux";
-
     environment.systemPackages = with pkgs; [
+      btop
+      duf
+      fzf
+      gdu
+      lf
+      lazygit
+      neovim
+      viddy
       ripgrep
-      fd
-      hello
+      sysz
+      tmux
+      nix-output-monitor
+      nixfmt-rfc-style
+      (writeShellScriptBin "mem" ''
+        viddy -n 0.5 -d --disable_auto_save '
+          sh -c "
+            echo $ free -h; free -h; echo;
+            echo $ zramctl; echo; zramctl; echo;
+            echo $ swapon --show; echo; swapon --show
+          "
+        '
+      '')
+      (writeShellScriptBin "dufi" ''
+        CLICOLOR_FORCE=1 COLORTERM="truecolor" viddy --disable_auto_save -p -d -n 0.5 \
+          "duf -only local,fuse -hide-mp /boot/efi -output \"mountpoint, avail, usage, inodes_avail, inodes_usage, filesystem\" -sort usage"
+      '')
     ];
-
     services.swapspace.enable = true;
-
     services.redis = {
       package = pkgs.valkey;
       servers.redrum = {
