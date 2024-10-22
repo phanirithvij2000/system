b834fe2d8750903313eda5b56370c5c21a54abf4
phanirithvij <phanirithvij2000@gmail.com>
2024-10-23 00:58:23 +0530

    tcount -> nur-packages
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/rithvij/default.nix
+++ home/rithvij/default.nix
@@ -23,7 +23,6 @@ in
     ../applications/hyprland.nix
     ../applications/rss.nix
     ../applications/shells
-    ../applications/tcount.nix
     ../applications/telegram
     ../applications/terminals.nix
     ../applications/tmux.nix
