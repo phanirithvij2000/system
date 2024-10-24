fd275ca893cca05d57c5df8158a722cc076a749d
phanirithvij <phanirithvij2000@gmail.com>
2024-10-24 23:09:08 +0530

    sysm upd [skip ci]
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- hosts/sysm/gha/configuration.nix
+++ hosts/sysm/gha/configuration.nix
@@ -6,34 +6,45 @@
   ];
   config = {
     nixpkgs.hostPlatform = "x86_64-linux";
-    environment.systemPackages = with pkgs; [
-      btop
-      duf
-      fzf
-      gdu
-      lf
-      lazygit
-      neovim
-      viddy
-      ripgrep
-      sysz
-      tmux
-      nix-output-monitor
-      nixfmt-rfc-style
-      (writeShellScriptBin "mem" ''
-        viddy -n 0.5 -d --disable_auto_save '
-          sh -c "
-            echo $ free -h; free -h; echo;
-            echo $ zramctl; echo; zramctl; echo;
-            echo $ swapon --show; echo; swapon --show
-          "
-        '
-      '')
-      (writeShellScriptBin "dufi" ''
-        CLICOLOR_FORCE=1 COLORTERM="truecolor" viddy --disable_auto_save -p -d -n 0.5 \
-          "duf -only local,fuse -hide-mp /boot/efi -output \"mountpoint, avail, usage, inodes_avail, inodes_usage, filesystem\" -sort usage"
-      '')
-    ];
+    environment.systemPackages =
+      # Now for macos (gha) maybe I can avoid system-manager
+      # and use the following via home-manager
+      with pkgs;
+      [
+        duf
+        fzf
+        gdu
+        lf
+        lazygit
+        neovim
+        ripgrep
+        tmux
+        viddy
+      ]
+      ++ [
+        nix-output-monitor
+        nixfmt-rfc-style
+        npins
+      ]
+      ++ [
+        (writeShellScriptBin "dufi" ''
+          CLICOLOR_FORCE=1 COLORTERM="truecolor" viddy --disable_auto_save -p -d -n 0.5 \
+            "duf -only local,fuse -hide-mp /boot/efi -output \"mountpoint, avail, usage, inodes_avail, inodes_usage, filesystem\" -sort usage"
+        '')
+      ]
+      ++ lib.optionals stdenv.hostPlatform.isLinux [
+        btop
+        (writeShellScriptBin "mem" ''
+          viddy -n 0.5 -d --disable_auto_save '
+            sh -c "
+              echo $ free -h; free -h; echo;
+              echo $ zramctl; echo; zramctl; echo;
+              echo $ swapon --show; echo; swapon --show
+            "
+          '
+        '')
+        sysz
+      ];
     services.swapspace.enable = true;
     services.redis = {
       package = pkgs.valkey;
