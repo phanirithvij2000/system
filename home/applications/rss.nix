9eb1a6aae7b1763b646314d050775c47e55af016
phanirithvij <phanirithvij2000@gmail.com>
2024-11-01 19:14:48 +0530

    rss hmm
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/rss.nix
+++ home/applications/rss.nix
@@ -1,41 +1,56 @@
-_: {
+{ pkgs, ... }:
+{
   programs.newsboat = {
     enable = true;
-    urls = builtins.map (x: { url = x; }) [
-      "https://invent.kde.org/multimedia/subtitlecomposer/-/tags?format=atom"
-      "https://github.com/YS-L/csvlens/releases.atom"
-      "https://github.com/thomiceli/opengist/releases.atom"
-      "https://github.com/sachaos/viddy/releases.atom"
-      "https://andrewkelley.me/rss.xml"
-      "https://bbengfort.github.io/index.xml"
-      "https://blog.adafruit.com/feed/"
-      "https://blog.janissary.xyz/feed.xml"
-      "https://blog.kowalczyk.info/atom.xml"
-      "https://bmcgee.ie/posts/index.xml"
-      "https://codewithoutrules.com/atom.xml"
-      "https://dwheeler.com/blog/index.rss"
-      "https://eli.thegreenplace.net/feeds/go.atom.xml"
-      "https://festivus.dev/index.xml"
-      "https://fmhy.net/feed.rss"
-      "https://gog-games.to/rss"
-      "http://habitatchronicles.com/feed/"
-      "https://lwn.net/headlines/rss"
-      "https://mitchellh.com/feed.xml"
-      "http://nil.wallyjones.com/feeds/all.atom.xml"
-      "https://perens.com/feed/"
-      "https://pixel-druid.com/feed.rss"
-      "https://python.libhunt.com/newsletter/feed"
-      "https://rootknecht.net/index.xml"
-      "https://rsapkf.org/weblog/rss.xml"
-      "https://sparkfun.com/feeds/news"
-      "https://terminaltrove.com/totw.xml"
-      "https://terminaltrove.com/new.xml"
-      "https://terminaltrove.com/blog.xml"
-      "https://this-week-in-rust.org/rss.xml"
-      "https://threedots.tech/index.xml"
-      "http://waywardmonkeys.org/feeds/all.atom.xml"
-      "https://www.trickster.dev/post/index.xml"
-      "https://ziglang.org/news/index.xml"
-    ];
+    urls = map (x: { url = x; }) (
+      (map (p: p.meta.homepage + "/releases.atom") (
+        with pkgs;
+        [
+          csvlens
+          opengist
+          viddy
+          gitcs
+          gogup
+          gomtree
+          gh-i
+          distrobox-tui
+        ]
+      ))
+      ++ [
+        "https://github.com/cloudcannon/pagefind/releases.atom" # homepage is not source url
+        "https://invent.kde.org/multimedia/subtitlecomposer/-/tags?format=atom"
+
+        "https://andrewkelley.me/rss.xml"
+        "https://bbengfort.github.io/index.xml"
+        "https://blog.adafruit.com/feed/"
+        "https://blog.janissary.xyz/feed.xml"
+        "https://blog.kowalczyk.info/atom.xml"
+        "https://bmcgee.ie/posts/index.xml"
+        "https://codewithoutrules.com/atom.xml"
+        "https://dwheeler.com/blog/index.rss"
+        "https://eli.thegreenplace.net/feeds/go.atom.xml"
+        "https://festivus.dev/index.xml"
+        "https://fmhy.net/feed.rss"
+        "https://gog-games.to/rss"
+        "http://habitatchronicles.com/feed/"
+        "https://lwn.net/headlines/rss"
+        "https://mitchellh.com/feed.xml"
+        "http://nil.wallyjones.com/feeds/all.atom.xml"
+        "https://perens.com/feed/"
+        "https://pixel-druid.com/feed.rss"
+        "https://python.libhunt.com/newsletter/feed"
+        "https://rootknecht.net/index.xml"
+        "https://rsapkf.org/weblog/rss.xml"
+        "https://sparkfun.com/feeds/news"
+        "https://terminaltrove.com/totw.xml"
+        "https://terminaltrove.com/new.xml"
+        "https://terminaltrove.com/blog.xml"
+        "https://this-week-in-rust.org/rss.xml"
+        "https://threedots.tech/index.xml"
+        "http://waywardmonkeys.org/feeds/all.atom.xml"
+        "https://www.trickster.dev/post/index.xml"
+        "https://ziglang.org/news/index.xml"
+      ]
+    );
   };
 }
