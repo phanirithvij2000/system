11205873bc6abb7cab5d76ef64009accba8e9c72
phanirithvij <phanirithvij2000@gmail.com>
2024-11-27 12:01:32 +0530

    tailscale disable
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/applications/tailscale.nix
+++ nixos/applications/tailscale.nix
@@ -2,7 +2,9 @@
 {
   environment.systemPackages = [ pkgs.tailscale ];
   services.tailscale = {
-    enable = true;
+    # TODO later, make nixos modules/config/settings thing
+    # so I can enable/disable them from ouside
+    enable = false;
     useRoutingFeatures = "client";
     openFirewall = true;
     extraUpFlags = [ "--operator=phanirithvij@github" ];
