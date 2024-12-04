f45dea6857c907364a3b4812ce2106b743426014
phanirithvij <phanirithvij2000@gmail.com>
2024-12-05 00:56:13 +0530

    mpv config WIP old dotfiles
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/media/default.nix
+++ home/applications/media/default.nix
@@ -7,7 +7,24 @@
   # TODO eliminate home-manager managed mpv
   # ideally home-manager shouldn't exist, it should be a one step deployment
   programs.mpv = {
-    package = pkgs.wrappedPkgs.mpv;
+    # Package is multutally exclusive with scripts? WTF
+    #package = pkgs.wrappedPkgs.mpv;
     enable = true;
+    config = {
+      auto-window-resize = false;
+      save-position-on-quit = true;
+    };
+    # https://github.com/mpv-player/mpv/blob/master/etc/input.conf
+    bindings = {
+      A = ''cycle-values video-aspect-override "2.35:1" "-1"'';
+      "ctrl+k" = "add sub-scale +0.1";
+      "ctrl+j" = "add sub-scale -0.1";
+      t = "script-message-to seek_to toggle-seeker";
+    };
+    scripts = with pkgs.mpvScripts; [
+      thumbfast
+      uosc
+      seekTo
+    ];
   };
 }
