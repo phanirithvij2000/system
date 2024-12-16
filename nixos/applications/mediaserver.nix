1fb5d04ad88fa314880c50a74101797c4747ac39
phanirithvij <phanirithvij2000@gmail.com>
2024-12-16 13:09:40 +0530

    enable navidrome
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/applications/mediaserver.nix
+++ nixos/applications/mediaserver.nix
@@ -1,4 +1,10 @@
 _: {
   services.jellyfin.enable = true;
   services.plex.enable = true;
+  services.navidrome = {
+    enable = true;
+    settings = {
+      MusicFolder = "/shed/music";
+    };
+  };
 }
