09ffd9dab1a1ccf7af8f569702f76e52f0adc68e
phanirithvij <phanirithvij2000@gmail.com>
2024-12-20 14:24:19 +0530

    espanso slight changes
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/bookmarks/espanso.nix
+++ home/applications/bookmarks/espanso.nix
@@ -1,7 +1,11 @@
 {
+  pkgs,
   wifipassFile,
   ...
 }:
+let
+  inherit (pkgs) lib xdotool;
+in
 {
   services.espanso = {
     enable = true;
@@ -11,7 +15,7 @@
     # gh auth token | xclip -sel clipboard
     # https://github.com/phanirithvij
     # https://github.com/phanirithvij/system
-    # @phanirithvij
+    # @phanirithvij, :me
     # private matches
     # TODO other crazy things
     # navi matches module
@@ -20,6 +24,8 @@
     # lesspass module
     # gopass module
     # buku module
+    # generate yaml no need for nix-fu
+    # replaceVars maybe and in wrappedPkgs
     matches = {
       base = {
         matches = [
@@ -29,6 +35,7 @@
           }
           {
             trigger = ":date";
+            label = "indian";
             replace = "{{mydate}}";
             vars = [
               {
@@ -43,12 +50,56 @@
           {
             trigger = ":date";
             replace = "{{mydate}}";
+            label = "lexico";
             vars = [
               {
                 name = "mydate";
                 type = "date";
                 params = {
                   format = "%Y-%m-%d";
+                };
+              }
+            ];
+          }
+          {
+            triggers = [
+              ":date"
+              ":now"
+            ];
+            replace = "{{mydate}}";
+            label = "lexico-time";
+            vars = [
+              {
+                name = "mydate";
+                type = "date";
+                params = {
+                  format = "%FT%T%:z";
+                };
+              }
+            ];
+          }
+          # https://pastebin.com/8wy9dAU3
+          # https://discord.com/channels/884163483409731584/1013914627886817372/1252015636876492882 (Espanso discord)
+          {
+            regex = "(?P<offset>[+-]\\d+)(?P<unit>[dwmy])";
+            label = "Bash offset date";
+            replace = "{{output}}";
+            vars = [
+              {
+                name = "output";
+                type = "shell";
+                params = {
+                  shell = "bash";
+                  cmd = # bash
+                    ''
+                      case {{unit}} in
+                        d) date=$(date -d "{{offset}} days" +"%Y-%m-%d") ;;
+                        w) date=$(date -d "({{offset}} * 7) days" +"%Y/%m/%d") ;;
+                        m) date=$(date -d "{{offset}} months" +"%Y/%m/%d") ;;
+                        y) date=$(date -d "{{offset}} years" +"%Y/%m/%d") ;;
+                      esac
+                      echo "$date"
+                    '';
                 };
               }
             ];
@@ -113,6 +164,78 @@
               https://github.com/avelino/awesome-go/blob/main/CONTRIBUTING.md#quality-standards
               > have at least 5 months of history since the first commit.
             '';
+          }
+          # https://github.com/espanso/espanso/discussions/1885#discussioncomment-8682979
+          # https://discord.com/channels/884163483409731584/1013914627886817372/1235217704986738709
+          # TODO: nix-prefetch-patch above with selected text maybe
+          {
+            trigger = "";
+            label = "Modify selected text CTRL+_";
+            replace = "{{output}}";
+            vars = [
+              {
+                name = null;
+                type = "shell";
+                params = {
+                  shell = "bash";
+                  cmd = "${lib.getExe xdotool} key --clearmodifiers ctrl+c";
+                };
+              }
+              {
+                name = "clipb";
+                type = "clipboard";
+              }
+              {
+                name = "script_choice";
+                type = "choice";
+                params = {
+                  values = [
+                    {
+                      label = "Uppercase";
+                      id = "upper";
+                    }
+                    {
+                      label = "Lowercase";
+                      id = "lower";
+                    }
+                    {
+                      label = "Initcase";
+                      id = "initcap";
+                    }
+                    {
+                      label = "Evaluate equation";
+                      id = "math";
+                    }
+                    {
+                      label = "Word count";
+                      id = "wordcount";
+                    }
+                  ];
+                };
+              }
+              {
+                name = "output";
+                type = "shell";
+                params = {
+                  shell = "bash";
+                  cmd = # bash
+                    ''
+                      case "{{script_choice}}" in
+                        upper)
+                          echo "{{clipb}}" | tr '[:lower:]' '[:upper:]';;
+                        lower)
+                          echo "{{clipb}}" | tr '[:upper:]' '[:lower:]';;
+                        initcap)
+                          awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1' <<< "{{clipb}}";;
+                        wordcount)
+                          echo "{{clipb}}" ; echo "{{clipb}}" | wc -w;;
+                        math)
+                          awk "BEGIN {print {{clipb}}}";;
+                      esac
+                    '';
+                };
+              }
+            ];
           }
         ];
       };
