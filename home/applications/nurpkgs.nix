f584d0bde0fbcd80fa88fbae096b1dc211a725bd
phanirithvij <phanirithvij2000@gmail.com>
2024-12-29 23:24:32 +0530

    skip installing overlayShell nur-pkgs
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/nurpkgs.nix
+++ home/applications/nurpkgs.nix
@@ -1,6 +1,17 @@
-{ flake-inputs, system, ... }:
 {
-  # ensure they all build?
-  # TODO ignore unstablePkgs
-  home.packages = builtins.attrValues flake-inputs.nur-pkgs.packages.${system};
+  flake-inputs,
+  lib,
+  system,
+  ...
+}:
+{
+  home.packages = builtins.attrValues (
+    lib.attrsets.filterAttrs (
+      n: _:
+      !builtins.elem n [
+        "overlayShell"
+        "unstablePkgs" # but it doesn't show up in packages.${system}
+      ]
+    ) flake-inputs.nur-pkgs.packages.${system}
+  );
 }
