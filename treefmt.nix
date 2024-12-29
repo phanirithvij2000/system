ef390e0df4df5d4d3dc6eac55a57c487b1fd4b95
phanirithvij <phanirithvij2000@gmail.com>
2024-12-29 11:27:26 +0530

    dprint try
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- treefmt.nix
+++ treefmt.nix
@@ -8,13 +8,27 @@ _: {
   programs.shfmt.enable = true;
   programs.shellcheck.enable = true;
 
-  programs.jsonfmt.enable = true;
-  programs.yamlfmt.enable = true;
-
-  # dprint broken with treefmt-nix, just install in nix-shell
-  #programs.dprint.enable = true;
+  programs.stylua.enable = true;
 
   # mdformat broken https://github.com/executablebooks/mdformat/issues/112
   # use manual dprint fmt
   #programs.mdformat.enable = true;
+
+  programs.dprint.enable = true;
+  programs.dprint.settings = {
+    includes = [
+      "**/*.{md,json,jsonc,toml,yml,yaml}"
+      "*.{md,json,jsonc,toml,yml,yaml}"
+    ];
+    excludes = [
+      "**/node_modules"
+      "**/*-lock.json"
+    ];
+    plugins = [
+      "https://plugins.dprint.dev/markdown-0.17.8.wasm"
+      "https://plugins.dprint.dev/json-0.19.4.wasm"
+      "https://plugins.dprint.dev/toml-0.6.3.wasm"
+      "https://plugins.dprint.dev/g-plane/pretty_yaml-v0.5.0.wasm"
+    ];
+  };
 }
