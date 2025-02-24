a90a1b3bbf6be9a1ac3f7a9785765827071de84e
phanirithvij <phanirithvij2000@gmail.com>
2025-02-24 18:42:59 +0530

    nurpkgs disable feedpushr
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/nurpkgs.nix
+++ home/applications/nurpkgs.nix
@@ -4,14 +4,21 @@
   system,
   ...
 }:
-{
-  home.packages = builtins.attrValues (
+let
+  #debug = lib.traceSeq (builtins.attrNames flake-inputs.nur-pkgs.packages.${system}) f;
+  f = builtins.attrValues (
     lib.attrsets.filterAttrs (
       n: _:
       !builtins.elem n [
         "overlayShell"
         "unstablePkgs" # but it doesn't show up in packages.${system}
+
+        # broken
+        "feedpushr"
       ]
     ) flake-inputs.nur-pkgs.packages.${system}
   );
+in
+{
+  home.packages = f;
 }
