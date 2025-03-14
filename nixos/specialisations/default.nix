a861250f80ff5bc080a1ba53b092a54c3c91f4fc
phanirithvij <phanirithvij2000@gmail.com>
2025-03-14 19:25:37 +0530

    save space turn off all DE specialisations
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- nixos/specialisations/default.nix
+++ nixos/specialisations/default.nix
@@ -4,13 +4,13 @@ _: {
   # and multiple modes to choose from in boot menu
 
   imports = [
-    ./cinnamon.nix
-    ./hyprland.nix
-    ./niri.nix
+    #./cinnamon.nix
+    #./hyprland.nix
+    #./niri.nix
     ./tty.nix
-    ./tuigreet.nix
+    #./tuigreet.nix
     #./xfce
-    ./plasma.nix
+    #./plasma.nix
 
     # NOTE specialisations cannot have '-' (OWN restriction, see the implementation in the below file)
     ../applications/scripts/home-manager-switch-specialisation.nix
