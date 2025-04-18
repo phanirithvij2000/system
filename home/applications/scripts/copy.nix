eab8c5c8748f3714d721e0a817e936ba588c8e8e
phanirithvij <phanirithvij2000@gmail.com>
2025-04-18 19:49:17 +0530

    paste command
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/scripts/copy.nix
+++ home/applications/scripts/copy.nix
@@ -8,5 +8,12 @@
         ${pkgs.lib.getExe pkgs.xclip} -sel clip
       fi
     '')
+    (pkgs.writeShellScriptBin "paste" ''
+      if [ -n "$WAYLAND_DISPLAY" ]; then
+        ${pkgs.lib.getExe pkgs.wl-clipboard-rs} -o -sel clip
+      else
+        ${pkgs.lib.getExe pkgs.xclip} -o -sel clip
+      fi
+    '')
   ];
 }
