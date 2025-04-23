04a20810356b441d6d3ac61962420e142e00bb2f
phanirithvij <phanirithvij2000@gmail.com>
2025-04-23 09:59:24 +0530

    tmux refactor
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/tmux.nix
+++ home/applications/tmux.nix
@@ -8,10 +8,12 @@ let
   '';
 in
 {
-  programs.tmux = {
-    enable = true;
-    package = pkgs.wrappedPkgs.tmux;
-  };
+  home.packages = [
+    pkgs.wrappedPkgs.tmux
+    pkgs.tmuxp
+    # dmux
+    # tmate?
+  ];
   programs.tmux-aliases.enable = true;
 
   /*
@@ -28,36 +30,15 @@ in
 
   programs.sesh = {
     enable = true;
+    # I copied what this flag does to my tmux.conf
     enableTmuxIntegration = false;
     settings = {
-      # TODO default_session is not working (not loading in new sesh sessions)
       default_session = {
+        # previously startup_script, but now a silent breaking change
         startup_command = "~/.config/sesh/sesh_startup_script.sh";
       };
     };
   };
 
   xdg.configFile."sesh/sesh_startup_script.sh".source = seshTmuxScript;
-
-  programs.tmux.extraConfig = ''
-    bind-key "g" run-shell "sesh connect \"$(
-      sesh list --icons | fzf-tmux -p 80%,70% \
-        --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
-        --header '  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find' \
-        --bind 'tab:down,btab:up' \
-        --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)' \
-        --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
-        --bind 'ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)' \
-        --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
-        --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
-        --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)' \
-        --preview-window 'right:55%' \
-        --preview 'sesh preview {}'
-        -- --ansi
-    )\""
-  '';
-
-  home.packages = [ pkgs.tmuxp ];
-  # dmux
-  # tmate?
 }
