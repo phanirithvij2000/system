8abd3d402a79ef3875787a763ce75a8443d147c0
phanirithvij <phanirithvij2000@gmail.com>
2025-04-27 11:20:11 +0530

    nh latest ver update overlay
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- lib/overlays/default.nix
+++ lib/overlays/default.nix
@@ -1,1 +1,29 @@
-_: [ ]
+_:
+let
+  nhLatestOverlay =
+    _: p:
+    let
+      targetVersion = "4.0.1";
+      version =
+        if (builtins.compareVersions p.nh.version targetVersion) >= 0 then
+          throw "nhLatestOverlay needs to be removed, nh version with hm sp support available ${p.nh.version}"
+        else
+          "4.0.2";
+      src = p.fetchFromGitHub {
+        owner = "viperML";
+        repo = "nh";
+        tag = "refs/tags/v${version}";
+        hash = "sha256-ajEl9nV4XFW7H98XCPUshzYL+K0+gVqEWUC4+MBAUFw=";
+      };
+    in
+    {
+      nh = p.nh.overrideAttrs (_: {
+        inherit version src;
+        cargoDeps = p.rustPlatform.fetchCargoVendor {
+          inherit src;
+          hash = "sha256-MqvYDCtj6omYpwhKvWkI5CRz8ZpT8OLj7SazJUzVtc8=";
+        };
+      });
+    };
+in
+[ nhLatestOverlay ]
