adbd8db58a31ca5b153c8b00252b04fe3efd7441
phanirithvij <phanirithvij2000@gmail.com>
2025-05-02 14:54:28 +0530

    todo tg-archive nixosmodule
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/applications/telegram/default.nix
+++ home/applications/telegram/default.nix
@@ -3,6 +3,8 @@
   home.packages = [
     pkgs.tdl
     pkgs.telegram-desktop
+    # TODO nixos module for this?
+    pkgs.tg-archive # https://github.com/knadh/tg-archive/issues/158
     # TODO teldrive?
     # but teldrive can't run as hm service since it requires postgres
     # TODO rclone declarative mount service
