0d7cbe7f8813660345979a486cdb921b57c2113e
phanirithvij <phanirithvij2000@gmail.com>
2025-05-14 08:43:40 +0530

    nod hm sops wifipassFile duplicate
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- home/users/nix-on-droid/default.nix
+++ home/users/nix-on-droid/default.nix
@@ -1,13 +1,24 @@
-_: {
+{ config, ... }@args:
+{
   imports = [
-    ../../applications/bookmarks/navi.nix
+    (import ../../applications/bookmarks/navi.nix (
+      args
+      // {
+        wifipassFile = config.sops.secrets.wifi_password_file.path;
+      }
+    ))
     ../../applications/git
     ../../applications/go
     ../../applications/nixy/nix.nix
     ../../applications/shells
     ../../applications/tmux.nix
   ];
+
+  # redefined for nix-on-droid user, to avoid espanso
+  # TODO espanso and navi should be decoupled
+  sops.secrets.wifi_password_file = { };
+
   home.username = "nix-on-droid";
   home.homeDirectory = "/data/data/com.termux.nix/files/home";
-  home.stateVersion = "23.11";
+  home.stateVersion = "24.11";
 }
