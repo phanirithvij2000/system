c9b116eb99defb44a1e8853af1db7330ce5e5a85
phanirithvij <phanirithvij2000@gmail.com>
2025-05-14 19:07:10 +0530

    sops try dynamic home dir
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,7 +1,11 @@
-_: {
+{ config, username, ... }:
+let
+  homeDir = if config ? home then config.home.homeDirectory else config.users.users.${username}.home;
+in
+{
   sops.defaultSopsFile = ./secrets.yml;
   sops.defaultSopsFormat = "yaml";
 
-  sops.age.sshKeyPaths = [ "/home/rithvij/.ssh/id_ed25519" ];
+  sops.age.sshKeyPaths = [ "${homeDir}/.ssh/id_ed25519" ];
   #sops.age.keyFile = "/home/user/.config/sops/age.key";
 }
